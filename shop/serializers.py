e1a68c6c9a9877c796b8b89bc2512e4a453800f1
nfrank75 <-->
2023-02-09 17:03:50 +0100

    Work in progres

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,22 +1,27 @@
-from rest_framework.serializers import ModelSerializer
- 
+from rest_framework.serializers import ModelSerializer 
 from shop.models import Category, Product, Article
+
  
-class CategorySerializer(ModelSerializer):
- 
+class ArticleSerializer(ModelSerializer):
     class Meta:
-        model = Category
-        fields = ['id', 'name']
+        model = Article
+        fields = '__all__'
+
 
 class ProductSerializer(ModelSerializer):
-
     class Meta:
         model = Product
         fields = '__all__'
 
 
-class ArticleSerializer(ModelSerializer):
-
+class CategorySerializer(ModelSerializer):
+    
+    products = ProductSerializer(many=True)
     class Meta:
-        model = Article
-        fields = '__all__'
+        model = Category
+        fields = ['id', 'name', 'description', 'active', 'created_at', 'updated_at', 'products']
+
+    def get_products(self, instance):
+        query_set = instance.products.filter(active=True)
+        serializer = ProductSerializer(query_set, many=True)
+        return serializer.data
